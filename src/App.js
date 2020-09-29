940be83e6867a8091fb1d0eee5c99414ac8e5fec
prrdee <18732242+prrdee@users.noreply.github.com>
2020-09-29 15:00:14 +0300

    Implement a function to create ChartData

--- src/App.js
+++ src/App.js
@@ -9,6 +9,49 @@ function App() {
     setInputText(event.target.value)
   }
 
+  const createChartData = text => {
+    //removing non alphabetic characters
+    const cleanString = text.toLowerCase().replace(/[^A-Za-z]/g, '')
+
+    const arrayOfArrays = Object.entries(
+      [...cleanString].reduce((obj, item) => {
+        if (item in obj) {
+          obj[item]++
+        } else {
+          obj[item] = 1
+        }
+        return obj
+      }, {}),
+    )
+
+    // map array of arrays to array of objects
+    const arrayOfObjects = arrayOfArrays.map(function ([a, b]) {
+      return { [a]: b }
+    })
+
+    // sort array of objects alphabetically
+    const result = arrayOfObjects.sort(function (a, b) {
+      if (Object.keys(a)[0] > Object.keys(b)[0]) return 1
+      return -1
+    })
+
+    // format result to create chartData
+    const labels = result.characterCount.map(element => Object.keys(element)[0])
+
+    const data = result.characterCount.map(element => Object.values(element)[0])
+
+    return {
+      labels: labels,
+      datasets: [
+        {
+          label: 'Character Count',
+          data: data,
+          backgroundColor: dynamicColors(data),
+        },
+      ],
+    }
+  }
+
   return (
     <div className="App">
       <h1>Analyze Text</h1>
