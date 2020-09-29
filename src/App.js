1203656333083f7af9f55c88a3d72e127d8b3246
prrdee <18732242+prrdee@users.noreply.github.com>
2020-09-29 14:54:21 +0300

    Create function to handle inputText change

--- src/App.js
+++ src/App.js
@@ -5,6 +5,10 @@ function App() {
   const [chartData, setChartData] = useState(null)
   const [inputText, setInputText] = useState('')
 
+  const handleInputTextChange = event => {
+    setInputText(event.target.value)
+  }
+
   return (
     <div className="App">
       <h1>Analyze Text</h1>
